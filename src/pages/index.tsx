10df669878e6668c58ffb3402e21a58184b6336e
Ivan Saldano <ivanms1@gmail.com>
2021-10-30 21:31:58 +0900

    add basic functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,76 @@
-import Head from "next/head";
+import { useEffect, useState } from "react";
+import { create, IPFS } from "ipfs-core";
 
-import Button from "../components/Button";
+function Home() {
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [fileHash, setFileHash] = useState<string | null | undefined>(null);
+  const [hashToDownload, setHashToDownload] = useState<
+    string | null | undefined
+  >(null);
+  const [id, setId] = useState<string | null>(null);
+  const [ipfs, setIpfs] = useState<IPFS | null>(null);
 
-import styles from "./index.module.scss";
+  const [isOnline, setIsOnline] = useState(false);
 
-export default function Home() {
+  useEffect(() => {
+    const init = async () => {
+      if (ipfs) return;
+
+      const node = await create();
+
+      const nodeId = await node.id();
+      const nodeIsOnline = node.isOnline();
+
+      setIpfs(node);
+      setId(nodeId?.id);
+      setIsOnline(nodeIsOnline);
+    };
+
+    init();
+  }, [ipfs]);
+
+  const handleUpload = async () => {
+    const added = await ipfs?.add(
+      {
+        path: file?.name,
+        content: file!,
+      },
+      { wrapWithDirectory: true }
+    );
+
+    setFileHash(added?.cid?.toString());
+  };
+
+  const handleDownload = async () => {
+    window.open(`https://ipfs.io/ipfs/${hashToDownload}`, "_blank");
+  };
+
+  if (!ipfs) {
+    return <h4>Connecting to IPFS...</h4>;
+  }
+
   return (
-    <div className={styles.Container}>
-      <Head>
-        <title>Next Template</title>
-        <meta name="description" content="Next.js template by Ivan" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Welcome</h1>
-      <Button>Click Me</Button>
+    <div>
+      <h4>ID: {id}</h4>
+      <h4>Status: {isOnline ? "Online" : "Offline"}</h4>
+      <div>
+        <input type="file" onChange={(e) => setFile(e.target?.files?.[0])} />
+        <button onClick={handleUpload}>Upload</button>
+
+        {fileHash && <h4>{fileHash}</h4>}
+      </div>
+      <hr />
+      <div>
+        <label htmlFor="download">Enter a hash</label>
+        <input
+          id="download"
+          onChange={(e) => setHashToDownload(e?.target?.value)}
+          type="text"
+        />
+        <button onClick={handleDownload}>Download</button>
+      </div>
     </div>
   );
 }
+
+export default Home;
